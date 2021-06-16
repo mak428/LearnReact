1c4539b8ea06dc3db9cace9d7c24ace9a2f41fb6
Muki <muki@Ashas-MacBook-Pro.local>
2021-06-16 13:46:04 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
+import React from 'react';
 import logo from './logo.svg';
+import { Greeting } from './Greeting';
+import { PeopleList} from './PeopleList';
 import './App.css';
 
+const people = [{
+  name: 'John',
+  age: 40,
+  hairColor: 'brown',
+}, {
+  name: 'Helga',
+  age: 25,
+  hairColor: 'red',
+},  {
+  name: 'Dwayne',
+  age: 55,
+  hairColor: 'blonde',
+}];
+
 function App() {
+  let adjective = "cool";
+  let url = 'https://reactjs.org';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Greeting name = "Shaun" numberOfMessages={0}/> 
+        <PeopleList people = {people}/>
+        <button onClick={() => alert('Hello')}>Click Me!</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is so {adjective}!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
